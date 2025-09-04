e01314d54d0d2856cc1af64eead88aea6434d285
AcciGen <145344775+AcciGen@users.noreply.github.com>
2025-09-04 11:16:54 +0500

    some changes

--- src/AdPlatforms.API/Program.cs
+++ src/AdPlatforms.API/Program.cs
@@ -26,7 +26,6 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
         app.UseAuthorization();
         app.MapControllers();
         app.Run();
